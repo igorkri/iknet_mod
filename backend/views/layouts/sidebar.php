2ab12c22547a19d520db5da5e770a69e94ad32ba
igorkri <igorkri26@gmail.com>
2023-04-15 09:48:52 +0300

    init repo

---
+++ backend/views/layouts/sidebar.php
@@ -0,0 +1,77 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+        <img src="<?=$assetDir?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">Iknet</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar user panel (optional) -->
+
+
+        <!-- SidebarSearch Form -->
+        <!-- href be escaped -->
+        <div class="form-inline">
+            <div class="input-group" data-widget="sidebar-search">
+                <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+                <div class="input-group-append">
+                    <button class="btn btn-sidebar">
+                        <i class="fas fa-search fa-fw"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <?php
+            echo \hail812\adminlte\widgets\Menu::widget([
+                'items' => [
+//                    [
+//                        'label' => 'Новости',
+//                        'icon' => 'tachometer-alt',
+////                        'badge' => '<span class="right badge badge-info">2</span>',
+//                        'items' => [
+//                            ['label' => 'Все новости', 'url' => ['news/index'], 'iconStyle' => 'far'],
+//                        ]
+//                    ],
+
+                    ['label' => 'Меню', 'url' => ['menu/index'], 'icon' => 'ellipsis-v'],
+                    ['label' => 'Категории', 'url' => ['menu/index'], 'icon' => 'bars'],
+                    ['label' => 'Новости', 'url' => ['news/index'], 'icon' => 'newspaper'],
+                    ['label' => 'Проекти', 'url' => ['project/index'], 'icon' => 'project-diagram'],
+                    ['label' => 'Цены и тарифы', 'url' => ['price/index'], 'icon' => 'barcode'],
+                    ['label' => 'Страницы', 'url' => ['price/index'], 'icon' => 'file-alt'],
+                    ['label' => 'Производители', 'url' => ['price/index'], 'icon' => 'copyright'],
+//                    ['label' => 'Gii',  'icon' => 'file-code', 'url' => ['/gii'], 'target' => '_blank'],
+//                    ['label' => 'Level1'],
+//                    [
+//                        'label' => 'Level1',
+//                        'items' => [
+//                            ['label' => 'Level2', 'iconStyle' => 'far'],
+//                            [
+//                                'label' => 'Level2',
+//                                'iconStyle' => 'far',
+//                                'items' => [
+//                                    ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle'],
+//                                    ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle'],
+//                                    ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle']
+//                                ]
+//                            ],
+//                            ['label' => 'Level2', 'iconStyle' => 'far']
+//                        ]
+//                    ],
+//                    ['label' => 'Level1'],
+//                    ['label' => 'LABELS', 'header' => true],
+//                    ['label' => 'Important', 'iconStyle' => 'far', 'iconClassAdded' => 'text-danger'],
+//                    ['label' => 'Warning', 'iconClass' => 'nav-icon far fa-circle text-warning'],
+//                    ['label' => 'Informational', 'iconStyle' => 'far', 'iconClassAdded' => 'text-info'],
+                ],
+            ]);
+            ?>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
