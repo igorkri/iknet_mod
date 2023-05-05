c25883fbd7fc5dc521a3d6e3d52eb7376992ee1c
igorkri <igorkri26@gmail.com>
2023-05-05 12:39:13 +0300

    new category table

---
+++ console/migrations/m230505_070846_create_news_category_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%news_category}}`.
+ */
+class m230505_070846_create_news_category_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%news_category}}', [
+            'id' => $this->primaryKey(),
+            'title_uk' => $this->string()->notNull()->comment('Название UK'),
+            'title_en' => $this->string()->notNull()->comment('Название EN'),
+            'title_ru' => $this->string()->notNull()->comment('Название RU'),
+            'slug' => $this->string()->unique()->comment('SLUG'),
+            'parent_id' => $this->integer()->comment('Родитель'),
+            'order' => $this->integer()->comment('Сортировка'),
+            'published' => $this->boolean()->defaultValue(true)->comment('Отображать'),
+            'image' => $this->string()->comment('Изображение'),
+            'seo_description_uk' => $this->string()->comment('SEO Описание UK'),
+            'seo_description_en' => $this->string()->comment('SEO Описание EN'),
+            'seo_description_ru' => $this->string()->comment('SEO Описание RU'),
+            'seo_keywords_uk' => $this->string()->comment('SEO Ключи UK'),
+            'seo_keywords_en' => $this->string()->comment('SEO Ключи EN'),
+            'seo_keywords_ru' => $this->string()->comment('SEO Ключи RU'),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%news_category}}');
+    }
+}
