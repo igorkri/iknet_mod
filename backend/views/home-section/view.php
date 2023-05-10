650a6022bd5d2a2fd1f7c88138412efe40a34426
jean <igorkri26@gmail.com>
2023-05-10 11:48:40 +0300

    admin  ***

--- backend/views/home-section/view.php
+++ backend/views/home-section/view.php
@@ -19,7 +19,7 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('Видалити', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
+                'confirm' => 'Ви впевнені, що хочете видалити цей елемент?',
                 'method' => 'post',
             ],
         ]) ?>
