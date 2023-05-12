ba333e3e3667757a6b2164caa65c77c96570f484
igorkri <igorkri26@gmail.com>
2023-05-12 07:21:52 +0300

    re captcha

--- frontend/controllers/CareerController.php
+++ frontend/controllers/CareerController.php
@@ -8,6 +8,7 @@ use common\models\PageCareer;
 use common\models\Vacancies;
 use Yii;
 use yii\base\BaseObject;
+use yii\helpers\VarDumper;
 use yii\web\Controller;
 use yii\web\UploadedFile;
 
@@ -21,8 +22,9 @@ class CareerController extends Controller
         $vacancies = Vacancies::find()->where(['published'=>1])->all();
 
         $post = \Yii::$app->request->post('FormCallback');
-
+        $post2 = \Yii::$app->request->post();
         if (Yii::$app->request->isPost) {
+        VarDumper::dump($post2, 10, true); die;
             if($_FILES['FormCallback']['size']['file'] > 0){
                 $dir = Yii::getAlias('@frontendWeb/career');
                 $file = UploadedFile::getInstance($model, 'file');
